8296bc05a2d1fcfafe0e7c8c3ec165c041303063
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-05-03 11:40:41 +0200

    Hello world! node-ts app

---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+greeter(user)
+console.log(greeter(user))
+
+//document.body.innerHTML = greeter(user);
